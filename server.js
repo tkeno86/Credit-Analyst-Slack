43c8937b576e67e34e5245faed0b8da9af859ad7
tkeno86 <thomas.kennedy986@gmail.com>
2025-06-05 22:50:29 +0100

    server

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 
 import slackRoutes from "./routes/slack.js";
 import oauthRoutes from "./routes/oauth.js";
+import slackDownload from "./routes/slackDownload.js";
 
 dotenv.config();
 
@@ -11,8 +12,13 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+  res.send("Slack bot backend is live!");
+});
+
 app.use("/slack", slackRoutes);
 app.use("/oauth", oauthRoutes);
+app.use("/api/slack/files", slackDownload);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`⚡ Server running on port ${PORT}`));
