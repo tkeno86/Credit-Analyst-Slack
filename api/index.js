0ffcc62c88f15ef893825ef606782598b4c4a154
tkeno86 <thomas.kennedy986@gmail.com>
2025-06-06 01:28:03 +0100

    ffs

--- api/index.js
+++ api/index.js
@@ -1,2 +1,21 @@
-import app from '../server.js';
+import express from "express";
+import dotenv from "dotenv";
+import slackDownload from "./routes/slackDownload.js";
+import slackParse from "./routes/slackParse.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use("/api/slack/files", slackDownload);
+app.use("/api/slack/files/parse", slackParse);
+
+app.get("/", (req, res) => {
+  res.send("Slack PDF Analyst backend is running.");
+});
+
+app.get("/favicon.ico", (req, res) => {
+  res.status(204).end();
+});
+
 export default app;
